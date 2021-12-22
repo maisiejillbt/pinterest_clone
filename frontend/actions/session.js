a2bfa8557870b37976424ee4ed57f61654d6f33d
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-12-22 11:55:39 -0800

    adding session errors and error reducer

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -2,25 +2,46 @@ import {
   postUser,
   postSession,
   deleteSession
- } from '../utils/session';
+} from '../utils/session';
 
- export const RECEIVE_CURRENT_USER = `RECEIVE_CURRENT_USER`;
- export const LOGOUT_CURRENT_USER = `LOGOUT_CURRENT_USER`;
+export const RECEIVE_CURRENT_USER = `RECEIVE_CURRENT_USER`;
+export const LOGOUT_CURRENT_USER = `LOGOUT_CURRENT_USER`;
+export const RECEIVE_SESSION_ERRORS = `RECEIVE_SESSION_ERRORS`;
+export const REMOVE_SESSION_ERRORS = `REMOVE_SESSION_ERRORS`; 
 
 const receiveCurrentUser = user => ({
   type:RECEIVE_CURRENT_USER,
   user,
-})
+});
 
 const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
-})
+});
 
-export const createNewUser = formUser => dispatch => postUser(formUser) 
-  .then(user => dispatch(receiveCurrentUser(user)));
+const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
 
-export const loginUser = formUser => dispatch => postSession(formUser) 
-  .then(user => dispatch(receiveCurrentUser(user)));
+export const createNewUser = formUser => dispatch => (
+    postUser(formUser).then(
+      user => 
+        dispatch(receiveCurrentUser(user)), 
+      err => 
+        dispatch(receiveErrors(err.responseJSON))
+    )
+  );
+
+export const loginUser = formUser => dispatch => (
+  postSession(formUser).then(
+    user => 
+      dispatch(receiveCurrentUser(user)),
+    err => 
+      dispatch(receiveErrors(err.responseJSON))
+  )
+);
 
 export const logoutUser = () => dispatch => deleteSession() 
   .then(() => dispatch(logoutCurrentUser())); 
+
+
