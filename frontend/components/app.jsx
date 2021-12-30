9b1c5df3d33f05ff74bd4e8052954a3a9200b3db
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-12-30 12:49:41 -0500

    housekeeping

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,37 +1,31 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import JackBlack from './home/test';
-import SignupContainer from './session/signup_container'
-import LoginContainer from './session/login_container'
-import CreatePinForm from './pins/create_pin_form_container'
-import CreateBoardForm from './boards/create_board_form_container'
-import UpdateBoardForm from './boards/update_board_form_container'
-import UpdatePinForm from './pins/update_pin_form_container'
-import PinIndexContainer from './pins/pins_container'
-import PinShowContainer from './pins/pin_show_container'
-import BoardContainer from './boards/board_index_container'
-import BoardShowContainer from './boards/board_show_container'
-import LandingPage from './home/landing_page'
-import UserShowContainer from './users/user_show_container'
+import { AuthRoute, ProtectedRoute } from '../utils/routes_utils'
 
-import NavBarAuthContainer from './nav_bar/nav_bar_auth_container'
+import LandingPage from './home/landing_page'
 import NavBarProtectedContainer from './nav_bar/nav_bar_protected_container'
 
-import { AuthRoute, ProtectedRoute } from '../utils/routes_utils'
+import LoginContainer from './session/login_container'
+import SignupContainer from './session/signup_container'
+
+import PinShowContainer from './pins/pin_show_container'
+import PinIndexContainer from './pins/pins_container'
+import UpdateBoardForm from './boards/update_board_form_container'
+import UpdatePinForm from './pins/update_pin_form_container'
+import BoardShowContainer from './boards/board_show_container'
+import UserShowContainer from './users/user_show_container'
+import CreatePinForm from './pins/create_pin_form_container'
+import CreateBoardForm from './boards/create_board_form_container'
 
 
 export default () => (
   <div>
     <AuthRoute path="/landing" component={LandingPage}/> 
+    <ProtectedRoute path="/" component={NavBarProtectedContainer}/> 
 
     <Switch>
       <AuthRoute exact path="/landing/login" component={LoginContainer} />
       <AuthRoute exact path="/landing/signup" component={SignupContainer}/> 
-    </Switch>
-
-    <ProtectedRoute path="/" component={NavBarProtectedContainer}/> 
-
-    <Switch>
       <ProtectedRoute exact path='/pins/:pinId' component={PinShowContainer}/>
       <ProtectedRoute exact path="/pins" component={PinIndexContainer}/>
       <ProtectedRoute path='/boards/:boardId/edit' component={UpdateBoardForm}/>
@@ -40,7 +34,6 @@ export default () => (
       <ProtectedRoute path='/users/:userId' component={UserShowContainer}/>
       <ProtectedRoute path='/create-pin' component={CreatePinForm}/>
       <ProtectedRoute path='/create-board' component={CreateBoardForm}/>
-     </Switch>
-    <Route path="/jackblack" component={JackBlack}/>   
+     </Switch> 
   </div>
 );
